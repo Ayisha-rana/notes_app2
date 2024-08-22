825adf6aa89e22d1ca5fb5c19637625aa3c7094b
K-Shibin <aslua3567@gmail.com>
2024-08-22 15:06:33 +0530

    controller added

--- lib/controller/controller.dart
+++ lib/controller/controller.dart
@@ -1,11 +1,13 @@
 class Controller {
-  static List<String> items=[
+  static List<Map<String,dynamic>> items=[
 
   ];
 
 
   void addItems({required String title, required String description, required String imagePath}){
 
-
+Map<String,dynamic> noteDetails = {'title':title,'description':description,'imagePath':imagePath};
+items.add(noteDetails);
+print(items);
   }
 }
