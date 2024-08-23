61b7f99391efe0180dc0488d9c6346c792ef624c
Aravind <AravindSagar2003@gmail.com>
2024-08-23 10:38:38 +0530

    hhh

--- lib/dbservice/dbservice.dart
+++ lib/dbservice/dbservice.dart
@@ -2,41 +2,29 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class Dbservice {
-  static Database ?database;
+  static Database? database;
 
-  Future<void> initialization()async{
+  Future<void> initialization() async {
     var databasesPath = await getDatabasesPath();
     String path = join(databasesPath, 'demo.db');
-    database=await openDatabase(
-        path,
-        onCreate: (db, version) {
-          return db.execute(
-              'CREATE TABLE note(title TEXT,description TEXT,image BLOB )'
-          );
-        },
-        version: 1
-
-    );
+    database = await openDatabase(path, onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE note(title TEXT,description TEXT,image BLOB )');
+    }, version: 1);
     print('ghjkvhkl');
-
-
-
-
-
-
   }
 
-  Future<void> insertToNote({required Map<String,dynamic> note})async{
-    try{
+  Future<void> insertToNote({required Map<String, dynamic> note}) async {
+    try {
       //
-      // database!.insert(table, values);
-
-    }
-    catch(e){
-
+      final a = await database!.insert(
+        'note',
+        note,
+        conflictAlgorithm: ConflictAlgorithm.replace,
+      );
+      print(a);
+    } catch (e) {print(e);
+      rethrow;
     }
   }
-
-
-
 }
